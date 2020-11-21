f6acadaa869470ad1f79a2873d020ca0c3af03d3
Roman Govor <roma-govor2013@ya.ru>
2020-11-22 01:42:09 +0300

    feat: add cards

---
+++ english-for-kids/src/index.js
@@ -0,0 +1,9 @@
+const switchCheckbox = document.querySelector(".switch__checkbox");
+const switchTrain = document.querySelector(".switch__train");
+const switchPlay = document.querySelector(".switch__play");
+
+switchCheckbox.addEventListener("change", () => {
+   switchPlay.classList.toggle("none");
+   switchTrain.classList.toggle("none");
+});
+
